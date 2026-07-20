94fbc71419e26b1a98f94b4d9e4264ac08f49fb1
Mikhail Davydenko <mimih1990@gmail.com>
2026-07-20 17:32:18 +0300

    pathfinder fix

--- taskBase.js
+++ taskBase.js
@@ -64,6 +64,30 @@ function creepCanDo(creep, type) {
     return true;
 }
 
+function makeCreepCostMatrix(roomName, self) {
+    var matrix = new PathFinder.CostMatrix();
+    var creeps = Game.rooms[roomName] ? Game.rooms[roomName].find(FIND_CREEPS) : [];
+    for (var i = 0; i < creeps.length; i++) {
+        var c = creeps[i];
+        if (c === self) continue;
+        if (!c.my) continue;
+        matrix.set(c.pos.x, c.pos.y, 0xff);
+        var nextDir = c.memory._nextDir;
+        if (nextDir !== undefined) {
+            var dx = [0, 0, 1, 1, 1, 0, -1, -1, -1][nextDir];
+            var dy = [-1, 1, -1, 0, 1, 0, 1, -1, 0][nextDir];
+            if (dx !== undefined) {
+                var nx = c.pos.x + dx;
+                var ny = c.pos.y + dy;
+                if (nx >= 0 && nx <= 49 && ny >= 0 && ny <= 49) {
+                    matrix.set(nx, ny, 0x80);
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
 function moveCreep(creep, target, opts) {
     if (!target) return;
     if (!target.pos) return;
@@ -85,6 +109,7 @@ function moveCreep(creep, target, opts) {
                     var mv = creep.move(dir);
                     if (mv === OK) {
                         pathCache.advance(key);
+                        creep.memory._nextDir = dir;
                         return;
                     }
                     if (mv === ERR_TIRED || mv === ERR_BUSY) return;
@@ -94,16 +119,26 @@ function moveCreep(creep, target, opts) {
         }
     }
 
+    var matrix = makeCreepCostMatrix(creep.pos.roomName, creep);
     var ret = PathFinder.search(
         creep.pos,
         { pos: target.pos, range: 1 },
-        { maxOps: 1500, plainCost: 2, swampCost: 10 }
+        {
+            maxOps: 1500,
+            plainCost: 2,
+            swampCost: 10,
+            roomCallback: function (rn) {
+                if (rn === creep.pos.roomName) return matrix;
+                return new PathFinder.CostMatrix();
+            },
+        }
     );
     if (!ret.incomplete && ret.path && ret.path.length > 0) {
         pathCache.set(key, ret.path);
         var d = creep.pos.getDirectionTo(ret.path[0]);
         if (d && d > 0) {
             creep.move(d);
+            creep.memory._nextDir = d;
             return;
         }
     }
